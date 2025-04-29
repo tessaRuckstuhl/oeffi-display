330bfc4d58c02a355476af8b05e6fa47cb61b20f
git <ge56fis@mytum.de>
2025-04-29 11:34:30 +0200

    initial setup

---
+++ src/main/java/com/example/display/controller/MVGController.java
@@ -0,0 +1,31 @@
+package com.example.display.controller;
+
+import com.example.display.model.MVGDeparture;
+import com.example.display.service.MVGService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class MVGController {
+    private final MVGService mvgService;
+    public MVGController(MVGService mvgService) {
+        this.mvgService = mvgService;
+    }
+    @GetMapping("/")
+    public String getMVG(Model model) {
+//        return mvgService.getDepartures();
+        List<MVGDeparture> departures = mvgService.getDepartures();
+        model.addAttribute("departures", departures);
+
+        LocalDateTime now = LocalDateTime.now();
+        String lastUpdated = now.getHour() + ":" + now.getMinute();
+        model.addAttribute("lastUpdated", lastUpdated);
+        return "index";
+    }
+
+}
